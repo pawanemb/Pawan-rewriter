02c4b48f38d04457e4ef25c5bfacff2cd8c70f0e
pawanemb <pawan.patra@emb.global>
2024-11-09 21:27:01 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="p-8">
+    <div className="p-4">
       <h1 className="text-2xl font-bold">Welcome to Blog Rewriter</h1>
     </div>
   )
