1914dd4c7432f2d92c06cf6427dcb31d232c5189
pawanemb <pawan.patra@emb.global>
2024-11-09 21:26:37 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <div className={`${inter.className} min-h-screen`}>
-      <Component {...pageProps} />
-    </div>
-  )
+  return <Component {...pageProps} />
 }
